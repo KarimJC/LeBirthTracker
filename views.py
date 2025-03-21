2878eeb911b4bd30cf5a273ebaa631975111f1b0
sotapoppin <shimizu.s@northeastern.edu>
2025-03-21 16:48:24 -0400

    Adds a stub for getting birthday
    views.py shows a page with a date input, /submit will take the post and just simply print the date

--- views.py
+++ views.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "this is the home page"
+    return render_template('websiteContents.html')
 
+@app.route("/submit", methods=["POST"])
+def submit():
+    date = request.form.get("birthday")
+    return date
 
 if __name__ == '__main__' :
     app.run(debug=True, port=8000)
