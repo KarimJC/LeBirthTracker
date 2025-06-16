8a0a1f21b1861ec7aed9bccb1b4abf9233b5a2c8
KarimJC <karimjcamara@gmail.com>
2025-06-16 14:31:27 -0400

    point counter between dates works again

--- scrape.py
+++ scrape.py
@@ -18,23 +18,25 @@ def player_day_points(player, date):
     list = client.player_box_scores(day=date.day, month=date.month, year=date.year)
 
     # TODO: rework the loop so that the loop ends when the player's name is found
-    # for i in list:
-    #     if player == i['name']:
-    #         return convert_to_points(i["made_field_goals"], 
-    #                                  i["made_three_point_field_goals"], 
-    #                                  i["made_free_throws"])
-    index = list.index(player)
-    return convert_to_points(list[index]["made_field_goals"], 
-                    list[index]["made_three_point_field_goals"], 
-                    list[index]["made_free_throws"])
+    for i in list:
+        if player == i['name']:
+            return convert_to_points(i["made_field_goals"], 
+                                    i["made_three_point_field_goals"], 
+                                    i["made_free_throws"])
+    # index = list.index("LeBron James")
+    # return convert_to_points(list[index]["made_field_goals"], 
+    #                 list[index]["made_three_point_field_goals"], 
+    #                 list[index]["made_free_throws"])
 
 # gets the total amount of points a player hsa scored between two dates (inclusive)
 # takes in a player (string), a start date and an end date
 def player_points_between_dates(player, start_date, end_date):
     current_date = start_date
     total_points = 0
-    while(current_date <= end_date):
-        total_points += player_day_points(player=player, date=current_date)
+    while current_date <= end_date:
+        day_points = player_day_points(player=player, date=current_date)
+        if day_points is not None:
+            total_points += day_points
         current_date += timedelta(days=1)
     
     return total_points
@@ -51,7 +53,8 @@ def full_season_points(player, year):
             return i['points']
         
 
-#print(client.search(term="Ko"))
+#print(client.search(term="Le"))
+#print(client.player_box_scores(day=22, month=4, year=2025))
 print(player_points_between_dates("LeBron James", date(2025, 4, 22), date(2025, 4, 27)))
 
 # for i in list:
